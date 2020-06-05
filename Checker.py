13aef9d93f2a1eebb497aedddda0cd5b59f0be74
Robert <robert.lukoshko@gmail.com>
2020-06-05 11:41:13 +0300

    More clean and sequence

--- Checker.py
+++ Checker.py
@@ -6,31 +6,27 @@ import schedule
 import datetime
 from joblib import Parallel, delayed
 
+release_date = '05.06.2020 13:00'
+ln = 'https://www.nike.com/ru/launch/t/air-max-90-pink-foam'
 
-#  Getting link and opening browser
+# When to run
+while 1:
+    date = datetime.datetime.now()
+    if date.day == 5 and date.month == 6:
+        break
+    else:
+        time.sleep(5)
 
 
 def nike(link):
     browser = webdriver.Chrome('/Users/rob/Programming/Checker/chromedriver')
     browser.get(link)
-
-    # b = []
-    # for i in range(n):
-    #     b.append(webdriver.Chrome('/Users/rob/Programming/Checker/chromedriver'))
-    #     b[i].get(link)
-    #
-    # for p in range(n):
-    #     size = b[p].find_elements_by_xpath('//button[text()[contains(.,"US 8.5")]]')
-    #     b[p].execute_script("arguments[0].scrollIntoView(true);", size[0])
-    #     size[0].click()
-
     time.sleep(3)
 
     #  Finding the element
     test = browser.find_elements_by_xpath('//button[text()[contains(.,"US 8.5")]]')
     browser.execute_script("arguments[0].scrollIntoView(true);", test[0])
     test[0].click()
-
     time.sleep(3)
 
     # Finding cart
@@ -43,19 +39,11 @@ def nike(link):
     browser.get('https://www.nike.com/ru/ru/cart')
     time.sleep(4)
 
+
     browser.close()
 
 
-#  We can automate it too
-release_date = '05.06.2020 13:00'
-l = 'https://www.nike.com/ru/launch/t/air-max-90-pink-foam'
+#  Running in parallel
+n = 3
 
-# When to run
-while 1:
-    date = datetime.datetime.now()
-    if date.day == 5 and date.month == 6:
-        break
-    else:
-        time.sleep(5)
-
-Parallel(n_jobs=-1)(delayed(nike)(l) for n in range(3))
+Parallel(n_jobs=-1)(delayed(nike)(ln) for i in range(n))
