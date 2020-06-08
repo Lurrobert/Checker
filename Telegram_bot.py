fe5a876e713a749820f412ff5c12c6a4801eca65
Robert <robert.lukoshko@gmail.com>
2020-06-08 11:20:28 +0300

    Bot is stable

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -9,6 +9,8 @@ from requests.exceptions import ConnectionError
 from uuid import uuid4
 import Checking
 import datetime
+from joblib import Parallel, delayed
+
 
 tok = '1194379393:AAG9NZwGoQZp8pdr_RkrHS9T8Ae_5vOkN64'
 bot = telegram.Bot(token=tok)
@@ -24,40 +26,37 @@ logger.setLevel(logging.INFO)
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text='Starting')
-    context.user_data['release_date'] = []
-    context.user_data['link'] = []
     context.user_data['credentials'] = []
 
 
 def receiving_data(update, context):
     message = str(update.message.text).splitlines()
     # if we don't have data now
-    if not context.user_data['release_date']:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='adding new')
-        try:
-            context.user_data['link'] = message[0]
-            # TODO add checking format of link and date
-            dt = message[1].split('.')
-            context.user_data['release_date'] = datetime.datetime(year=int(dt[0]), month=int(dt[1]), day=int(dt[2]))
-        except:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='Wrong format. Try again')
-    else:
-        values = message
-        forms = ['Shipping_LastName', 'Shipping_FirstName', 'Shipping_MiddleName', 'Shipping_PostCode',
-                 'Shipping_Region', 'Shipping_Address1', 'Shipping_Address2', 'Shipping_phonenumber',
-                 'shipping_Email', 'idNumber', 'IdIssuingAuthority', 'IdVatNumber', 'card_number',
-                 'expiry_month', 'expiry_year', 'cvv', 'Size']
+    values = message
+    forms = ['link', 'date', 'size', 'Shipping_LastName', 'Shipping_FirstName', 'Shipping_MiddleName',
+             'Shipping_PostCode',
+             'Shipping_Region', 'Shipping_Address1', 'Shipping_Address2', 'Shipping_phonenumber',
+             'shipping_Email', 'idNumber', 'IdIssuingAuthority', 'IdVatNumber', 'card_number',
+             'expiry_month', 'expiry_year', 'cvv']
 
-        context.user_data['credentials'].append(dict(zip(forms, values)))
+    context.user_data['credentials'].append(dict(zip(forms, values)))
 
 
 def start_checking(update, context):
-    link = context.user_data['link']
-    release_date = context.user_data['release_date']
     credentials = context.user_data['credentials']
-    Checking.start(link, credentials, release_date)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Running')
+    Checking.start(credentials)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Everything is done')
+
+
+def stoper(update, context):
+    if str.lower(str(update.message.text)) == 'stop':
+        print('working')
+        updater.stop()
+        updater.start_polling()
+
 
 
 start_handler = CommandHandler('start', start)
